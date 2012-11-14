bf0bdf1853de126fc2c8b4efb88e7c48e2fefda6
cf <cf@cfserv.(none)>
2012-11-14 13:42:08 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personal::Application.config.secret_token = 'bb9decc20cf168b415602209eba5e9ebaa960f730a95175ed33a3548124600931fd9716f3d6eab05a1ad4a4157f63adb9efc671f63ac1c77d6880cab88f3198c'
